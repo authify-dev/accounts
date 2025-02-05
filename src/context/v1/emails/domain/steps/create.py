119a835d77de5dd217a891ad830743961bf95cd9
reitmas32 <rafa.zamora.rals@gmail.com>
2025-02-05 02:09:13 -0600

    feat: add email uniqueness validation in CreateEmailStep

--- src/context/v1/emails/domain/steps/create.py
+++ src/context/v1/emails/domain/steps/create.py
@@ -2,6 +2,7 @@ from context.v1.emails.domain.entities.email import EmailEntity
 from context.v1.users.domain.entities.user import UserEntity
 from shared.app.controllers.saga.controller import StepSAGA
 from shared.app.errors.invalid.type_invalid import TypeInvalidError
+from shared.app.errors.uniques.email_unique import EmailUniqueError
 from shared.databases.infrastructure.repository import RepositoryInterface
 
 
@@ -9,11 +10,16 @@ class CreateEmailStep(StepSAGA):
     def __init__(self, entity: EmailEntity, repository: RepositoryInterface):
         self.entity = entity
         self.repository = repository
+        self.email = None
 
     def __call__(self, payload: None = None, all_payloads: dict | None = None):  # noqa: ARG002
         self.email = None
         if type(payload) is not UserEntity:
             raise TypeInvalidError(valid_type=UserEntity, invalid_type=type(payload))
+
+        current_users_with_email = self.repository.get_by_attributes(filters={"email": self.entity.email}, limit=1)
+        if len(current_users_with_email) > 0:
+            raise EmailUniqueError(email=self.entity.email)
         self.entity.user_id = payload.id
 
         self.email = self.repository.add(**self.entity.model_dump())
