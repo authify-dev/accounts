8c603739880f4d0379a6fbc77a3138aa6d6a3798
reitmas32 <rafa.zamora.rals@gmail.com>
2025-02-20 22:46:40 -0600

    feat: add Filters method to RolePostgresRepository for role filtering based on criteria

--- internal/db/roles/postgres/repository.go
+++ internal/db/roles/postgres/repository.go
@@ -2,7 +2,9 @@ package postgres
 
 import (
 	"accounts/internal/api/v1/roles/domain/entities"
+	"accounts/internal/common/criteria"
 	base_entities "accounts/internal/core/domain"
+	"fmt"
 
 	"gorm.io/gorm"
 )
@@ -38,6 +40,26 @@ func (u *RolePostgresRepository) List() ([]entities.Role, error) {
 				CreatedAt: role.CreatedAt,
 			},
 		})
+	}
+	return rolesEntities, nil
+}
+
+func (u *RolePostgresRepository) Filters(crit criteria.Criteria) ([]entities.Role, error) {
+	var roles []RoleModel
+	var rolesEntities []entities.Role
+	for _, filter := range crit.Filters {
+		query := fmt.Sprintf("%s %s ?", filter.Field, filter.Operator)
+		u.Conection.Where(query, filter.Value).Find(&roles)
+		for _, role := range roles {
+			rolesEntities = append(rolesEntities, entities.Role{
+				Name:        role.Name,
+				Description: role.Description,
+				Entity: base_entities.Entity{
+					ID:        role.ID,
+					CreatedAt: role.CreatedAt,
+				},
+			})
+		}
 	}
 	return rolesEntities, nil
 }
--- internal/db/roles/repository.go
+++ internal/db/roles/repository.go
@@ -1,8 +1,12 @@
 package roles
 
-import "accounts/internal/api/v1/roles/domain/entities"
+import (
+	"accounts/internal/api/v1/roles/domain/entities"
+	"accounts/internal/common/criteria"
+)
 
 type RolesRepository interface {
 	Save(user entities.Role) error
 	List() ([]entities.Role, error)
+	Filters(crit criteria.Criteria) ([]entities.Role, error)
 }
