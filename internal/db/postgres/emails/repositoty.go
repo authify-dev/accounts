806e760f0906b1550b30974dd5ade49f0b308688
reitmas32 <rafa.zamora.rals@gmail.com>
2025-02-23 19:22:38 -0600

    feat: implement EmailPostgresRepository for email data handling with criteria matching

--- internal/db/postgres/emails/repositoty.go
+++ internal/db/postgres/emails/repositoty.go
@@ -1,1 +1,34 @@
 package postgres
+
+import (
+	"accounts/internal/api/v1/emails/domain/entities"
+	"accounts/internal/core/domain/criteria"
+	"accounts/internal/db/postgres"
+
+	"gorm.io/gorm"
+)
+
+// --------------------------------
+// INFRASTRUCTURE
+// --------------------------------
+// Role Postgres Repository
+// --------------------------------
+
+type EmailPostgresRepository struct {
+	postgres.PostgresRepository[entities.Email, EmailModel]
+}
+
+func NewEmailPostgresRepository(connection *gorm.DB) *EmailPostgresRepository {
+	return &EmailPostgresRepository{
+		PostgresRepository: postgres.PostgresRepository[entities.Email, EmailModel]{
+			Connection: connection,
+		},
+	}
+}
+
+func (r *EmailPostgresRepository) Matching(cr criteria.Criteria) ([]entities.Email, error) {
+
+	model := &EmailModel{}
+
+	return r.MatchingLow(cr, model)
+}
