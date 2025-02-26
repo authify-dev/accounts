63a49716abaa5bf3b67589b4c5b8ff4aedc00e2d
reitmas32 <rafa.zamora.rals@gmail.com>
2025-02-25 19:57:57 -0600

    feat: implement CreateUserStep for user creation and role validation

---
+++ internal/api/v1/emails/domain/steps/create_user.go
@@ -0,0 +1,101 @@
+package steps
+
+import (
+	roles "accounts/internal/api/v1/roles/domain/repositories"
+	"accounts/internal/api/v1/users/domain/entities"
+	users "accounts/internal/api/v1/users/domain/repositories"
+	"accounts/internal/common/logger"
+	"accounts/internal/core/domain/criteria"
+	"accounts/internal/utils"
+	"context"
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+type CreateUserStep struct {
+	user_id   uuid.UUID
+	user_repo users.UserRepository
+	role_repo roles.RoleRepository
+	user      entities.User
+}
+
+func NewCreateUserStep(
+	user_repo users.UserRepository,
+	role_repo roles.RoleRepository,
+	user entities.User,
+) *CreateUserStep {
+	return &CreateUserStep{
+		user_repo: user_repo,
+		role_repo: role_repo,
+		user:      user,
+	}
+}
+
+func (s *CreateUserStep) Call(ctx context.Context, payload utils.Result[any], allPayloads map[string]utils.Result[any]) utils.Result[any] {
+
+	entry := logger.FromContext(ctx)
+
+	// Verificar ID del role
+	criteria := criteria.Criteria{
+		Filters: *criteria.NewFilters(
+			[]criteria.Filter{
+				{
+					Field:    "name",
+					Value:    s.user.Role,
+					Operator: criteria.OperatorEqual,
+				},
+			},
+		),
+	}
+
+	roles, err := s.role_repo.Matching(criteria)
+	if err != nil {
+		entry.Error("error matching role")
+		return utils.Result[any]{Err: err}
+	}
+
+	if len(roles) == 0 {
+		entry.Error("role not found")
+		return utils.Result[any]{Err: errors.New("role not found")}
+	}
+
+	role_id := roles[0].ID
+
+	// Crear usuario
+	s.user.RoleID = role_id.String()
+
+	err = s.user_repo.Save(s.user)
+	if err != nil {
+		entry.Error("error saving user")
+		return utils.Result[any]{Err: err}
+	}
+
+	s.user_id = s.user.ID
+
+	return utils.Result[any]{
+		Data: s.user,
+	}
+}
+
+func (s *CreateUserStep) Rollback(ctx context.Context) error {
+	entry := logger.FromContext(ctx)
+
+	// Implementación de la lógica de negocio
+	if s.user_id == uuid.Nil {
+		entry.Error("user_id is empty")
+		return nil
+	}
+
+	if err := s.user_repo.Delete(s.user_id.String()); err != nil {
+		entry.Error("error deleting user")
+		return err
+	}
+
+	entry.Info("user deleted")
+	return nil
+}
+
+func (s *CreateUserStep) Produce() string {
+	return "entities.User"
+}
