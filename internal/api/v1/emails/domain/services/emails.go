eb403db0f3ecfdf8d7fc847f5515943821ab14e8
reitmas32 <rafa.zamora.rals@gmail.com>
2025-02-27 21:59:04 -0600

    feat: add codes_repository to EmailsService for code management integration

--- internal/api/v1/emails/domain/services/emails.go
+++ internal/api/v1/emails/domain/services/emails.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	codes "accounts/internal/api/v1/codes/domain/repositories"
 	emails "accounts/internal/api/v1/emails/domain/repositories"
 	login_methods "accounts/internal/api/v1/login_methods/domain/repositories"
 	roles "accounts/internal/api/v1/roles/domain/repositories"
@@ -12,6 +13,7 @@ type EmailsService struct {
 	user_repository          users.UserRepository
 	role_repository          roles.RoleRepository
 	login_methods_repository login_methods.LoginMethodRepository
+	codes_repository         codes.CodeRepository
 }
 
 func NewEmailsService(
@@ -19,11 +21,13 @@ func NewEmailsService(
 	user_repository users.UserRepository,
 	role_repository roles.RoleRepository,
 	login_methods_repository login_methods.LoginMethodRepository,
+	codes_repository codes.CodeRepository,
 ) *EmailsService {
 	return &EmailsService{
 		repository:               repository,
 		user_repository:          user_repository,
 		role_repository:          role_repository,
 		login_methods_repository: login_methods_repository,
+		codes_repository:         codes_repository,
 	}
 }
