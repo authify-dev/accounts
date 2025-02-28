793c95323f3e0ebeb60fe2e418956ca45537e768
reitmas32 <rafa.zamora.rals@gmail.com>
2025-02-27 22:41:56 -0600

    feat: add JSON serialization and JWT generation methods to LoginMethod and RefreshToken entities

--- internal/api/v1/login_methods/domain/entities/login_method.go
+++ internal/api/v1/login_methods/domain/entities/login_method.go
@@ -1,7 +1,10 @@
 package entities
 
 import (
+	jwt_controller "accounts/internal/common/controllers"
 	"accounts/internal/core/domain"
+	"accounts/internal/core/settings"
+	"encoding/json"
 )
 
 // --------------------------------
@@ -15,4 +18,33 @@ type LoginMethod struct {
 	domain.Entity
 	UserID   string `json:"user_id,omitempty"`
 	EntityID string `json:"entity_id,omitempty"`
+}
+
+func (r LoginMethod) ToJSON() map[string]interface{} {
+	// Convertir el struct a JSON.
+	data, err := json.Marshal(r)
+	if err != nil {
+		return nil
+	}
+
+	// Convertir los bytes JSON a un mapa.
+	var result map[string]interface{}
+	if err := json.Unmarshal(data, &result); err != nil {
+		return nil
+	}
+
+	return result
+}
+
+func (r LoginMethod) ToJWT(jwt_controller jwt_controller.JWTController) string {
+	login_map := r.ToJSON()
+
+	delete(login_map, "updated_at")
+	delete(login_map, "created_at")
+
+	jwt, err := jwt_controller.GenerateToken(login_map, settings.Settings.JWT_EXPIRE)
+	if err != nil {
+		return ""
+	}
+	return jwt
 }
--- internal/api/v1/refresh_tokens/domain/entities/login_method.go
+++ internal/api/v1/refresh_tokens/domain/entities/login_method.go
@@ -1,7 +1,10 @@
 package entities
 
 import (
+	jwt_controller "accounts/internal/common/controllers"
 	"accounts/internal/core/domain"
+	"accounts/internal/core/settings"
+	"encoding/json"
 	"time"
 )
 
@@ -19,4 +22,35 @@ type RefreshToken struct {
 	ExternalID    string    `json:"external_id,omitempty"`
 	ExpiresAt     time.Time `json:"expires_at,omitempty"`
 	RemoveAt      time.Time `json:"remove_at,omitempty"`
+}
+
+func (r RefreshToken) ToJSON() map[string]interface{} {
+	// Convertir el struct a JSON.
+	data, err := json.Marshal(r)
+	if err != nil {
+		return nil
+	}
+
+	// Convertir los bytes JSON a un mapa.
+	var result map[string]interface{}
+	if err := json.Unmarshal(data, &result); err != nil {
+		return nil
+	}
+
+	return result
+}
+
+func (r RefreshToken) ToJWT(jwt_controller jwt_controller.JWTController) string {
+	refresh_map := r.ToJSON()
+
+	delete(refresh_map, "updated_at")
+	delete(refresh_map, "created_at")
+	delete(refresh_map, "user_id")
+	delete(refresh_map, "login_method_id")
+
+	jwt, err := jwt_controller.GenerateToken(refresh_map, settings.Settings.REFRESH_EXPIRE)
+	if err != nil {
+		return ""
+	}
+	return jwt
 }
