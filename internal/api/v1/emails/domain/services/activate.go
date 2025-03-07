829ecb6eaa20129dc659a9cff95d21eedb8ecf65
reitmas32 <rafa.zamora.rals@gmail.com>
2025-03-07 05:51:37 -0600

    feat: enhance activation process by generating JWT and RefreshToken, and adding entity type filter

--- internal/api/v1/emails/domain/services/activate.go
+++ internal/api/v1/emails/domain/services/activate.go
@@ -2,27 +2,24 @@ package services
 
 import (
 	"accounts/internal/api/v1/emails/domain/entities"
+	"accounts/internal/common/logger"
+	"accounts/internal/core/domain"
 	"accounts/internal/core/domain/criteria"
 	"accounts/internal/utils"
 	"context"
 	"fmt"
 	"time"
+
+	logins "accounts/internal/api/v1/login_methods/domain/entities"
+	refreshs "accounts/internal/api/v1/refresh_tokens/domain/entities"
+
+	"github.com/google/uuid"
 )
 
 func (s *EmailsService) Activate(
 	ctx context.Context,
 	entity entities.Activate,
 ) utils.Responses[entities.ActivateResponse] {
-
-	// TODO: añadir el filed used_at al code y tipo de code
-
-	// buscar el id del email
-	// buscar el id del login con el email
-	// buscar el code del login
-	// comparar el code del login con el code del request
-
-	// Generar JWT
-	// Generar RefreshToken
 
 	criteria_email := criteria.Criteria{
 		Filters: *criteria.NewFilters(
@@ -58,6 +55,11 @@ func (s *EmailsService) Activate(
 				{
 					Field:    "entity_id",
 					Value:    email.ID,
+					Operator: criteria.OperatorEqual,
+				},
+				{
+					Field:    "entity_type",
+					Value:    "email",
 					Operator: criteria.OperatorEqual,
 				},
 			},
@@ -128,11 +130,71 @@ func (s *EmailsService) Activate(
 		}
 	}
 
+	s.login_methods_repository.UpdateByFields(login.ID, map[string]interface{}{
+		"is_verify": true,
+	})
+
+	refreshs_result := s.createRefreshToken(ctx, login)
+
+	if refreshs_result.Err != nil {
+		return utils.Responses[entities.ActivateResponse]{
+			StatusCode: 500,
+			Errors:     []string{refreshs_result.Err.Error()},
+		}
+	}
+
+	result := s.generateTokens(login, refreshs_result.Data)
+
+	if result.Err != nil {
+		return utils.Responses[entities.ActivateResponse]{
+			StatusCode: 500,
+			Errors:     []string{result.Err.Error()},
+		}
+	}
+
 	return utils.Responses[entities.ActivateResponse]{
 		StatusCode: 200,
 		Body: entities.ActivateResponse{
-			JWT:          "jwt",
-			RefreshToken: "refresh",
+			JWT:          result.Data.jwt,
+			RefreshToken: result.Data.refresh_token,
 		},
+	}
+}
+
+func (s EmailsService) generateTokens(login logins.LoginMethod, refreshToken refreshs.RefreshToken) utils.Either[GenerateTokensFlow] {
+
+	jwt := login.ToJWT(s.jwt_controller)
+
+	refresh_token := refreshToken.ToJWT(s.jwt_controller)
+
+	return utils.Either[GenerateTokensFlow]{Data: GenerateTokensFlow{
+		jwt:           jwt,
+		refresh_token: refresh_token,
+	}}
+}
+
+func (s EmailsService) createRefreshToken(ctx context.Context, login logins.LoginMethod) utils.Either[refreshs.RefreshToken] {
+
+	entry := logger.FromContext(ctx)
+
+	external_id := uuid.New()
+
+	entity := refreshs.RefreshToken{
+		UserID:        login.UserID,
+		Entity:        domain.Entity{},
+		LoginMethodID: login.ID,
+		ExternalID:    external_id.String(),
+	}
+
+	result := s.refresh_repository.Save(entity)
+	if result.Err != nil {
+		entry.Error("error saving the code")
+		return utils.Either[refreshs.RefreshToken]{Err: result.Err}
+	}
+
+	entity.ID = result.Data
+
+	return utils.Either[refreshs.RefreshToken]{
+		Data: entity,
 	}
 }
