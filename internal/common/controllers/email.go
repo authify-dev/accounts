407e0f8390a0b48e249bbb46149ca053fedc881c
reitmas32 <rafa.zamora.rals@gmail.com>
2025-03-15 14:10:49 -0600

    feat: enhance email sending functionality with headers and proper message formatting

--- internal/common/controllers/email.go
+++ internal/common/controllers/email.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"log"
 	"net/smtp"
 )
@@ -22,14 +23,29 @@ func NewEmailController(sender, password, smtp_host, smtp_port string) EmailCont
 }
 
 func (ec EmailController) SendEmail(to, subject, body string) {
+	// Construir los encabezados del email
+	headers := make(map[string]string)
+	headers["From"] = ec.sender
+	headers["To"] = to
+	headers["Subject"] = subject
+	headers["MIME-Version"] = "1.0"
+	headers["Content-Type"] = `text/html; charset="UTF-8"`
 
-	mensaje := []byte(subject + "\r\n" + body)
+	// Crear el mensaje concatenando los headers y el body
+	message := ""
+	for k, v := range headers {
+		message += fmt.Sprintf("%s: %s\r\n", k, v)
+	}
+	message += "\r\n" + body
+
+	// Convertir el mensaje a []byte
+	msg := []byte(message)
 
 	// Configurar la autenticación SMTP
 	auth := smtp.PlainAuth("", ec.sender, ec.password, ec.smtp_host)
 
 	// Enviar el correo
-	err := smtp.SendMail(ec.smtp_host+":"+ec.smtp_port, auth, ec.sender, []string{to}, mensaje)
+	err := smtp.SendMail(ec.smtp_host+":"+ec.smtp_port, auth, ec.sender, []string{to}, msg)
 	if err != nil {
 		log.Printf("Error al enviar el email: %v", err)
 		return
