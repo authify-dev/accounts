96875e21b2409d84fe37bd26a6c3eb491a82d7be
reitmas32 <rafa.zamora.rals@gmail.com>
2025-03-31 22:08:10 -0600

    feat: enhance reset password functionality with logging and DTO handling

--- internal/api/v1/emails/interface/controllers/password.go
+++ internal/api/v1/emails/interface/controllers/password.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"accounts/internal/api/v1/emails/interface/dtos"
+	"accounts/internal/common/logger"
+	"accounts/internal/common/requests"
 	"accounts/internal/common/responses"
 
 	"github.com/gin-gonic/gin"
@@ -8,9 +11,26 @@ import (
 )
 
 func (c *EmailsController) ResetPassword(ctx *gin.Context) {
+
+	entry := logger.FromContext(ctx.Request.Context())
+
+	entry.Info("ResetPassword")
+
+	dto := requests.GetDTO[dtos.ResetPasswordDTO](ctx)
+
+	entity := dto.ToEntity()
+	entry.Infof("DTO: %v", dto)
+	entry.Infof("Entity: %v", entity)
+
+	response := c.userService.ResetPassword(ctx.Request.Context(), entity)
+
+	ctx.JSON(response.StatusCode, response.ToMap())
+}
+
+func (c *EmailsController) ResetPasswordConfirm(ctx *gin.Context) {
 	customResponse := responses.Response{
 		Status: fiber.StatusOK,
-		Data:   "ResetPassword",
+		Data:   "ResetPasswordConfirm",
 	}
 
 	// Se almacena el objeto para que el middleware lo procese
