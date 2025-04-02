9ebb8368996c0b281a322106199a47f403e6e2fb
reitmas32 <rafa.zamora.rals@gmail.com>
2025-04-01 18:19:29 -0600

    feat: add ConfirmPasswordDTO structure with validation and JSON conversion methods

--- internal/api/v1/emails/interface/dtos/password.go
+++ internal/api/v1/emails/interface/dtos/password.go
@@ -27,3 +27,30 @@ func (dto ResetPasswordDTO) ToEntity() entities.ResetPassword {
 		Email: dto.Email,
 	}
 }
+
+type ConfirmPasswordDTO struct {
+	Email    string `json:"email" binding:"required,email"`
+	Code     string `json:"code" binding:"required,min=6,max=6"`
+	Password string `json:"password" binding:"required,min=8,max=100"`
+}
+
+func (dto ConfirmPasswordDTO) Validate() error {
+	return nil
+}
+
+func (dto ConfirmPasswordDTO) ToJson() []byte {
+	data, err := json.Marshal(dto)
+	if err != nil {
+		return []byte{}
+	}
+
+	return data
+}
+
+func (dto ConfirmPasswordDTO) ToEntity() entities.ConfirmPassword {
+	return entities.ConfirmPassword{
+		Email:    dto.Email,
+		Code:     dto.Code,
+		Password: dto.Password,
+	}
+}
