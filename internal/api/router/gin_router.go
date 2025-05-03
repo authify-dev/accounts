e50de3ae4f269044461b6c7e60fb883285ac1372
reitmas32 <rafa.zamora.rals@gmail.com>
2025-05-02 22:23:37 -0600

    feat: add request logging middleware to capture and log request and response details

--- internal/api/router/gin_router.go
+++ internal/api/router/gin_router.go
@@ -1,11 +1,15 @@
 package router
 
 import (
+	"accounts/internal/common/middlewares"
+
 	"github.com/gin-gonic/gin"
 )
 
 func NewRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(middlewares.RequestLogMiddleware())
 
 	return r
 }
