3280235855eb17331768dee08dca509d57ea7131
reitmas32 <rafa.zamora.rals@gmail.com>
2025-05-06 01:45:36 -0600

    feat: add Role field to SigninGoogleDTO for enhanced user role management

--- internal/api/v1/oauth_logins/interface/dtos/signin.go
+++ internal/api/v1/oauth_logins/interface/dtos/signin.go
@@ -2,6 +2,7 @@ package dtos
 
 type SigninGoogleDTO struct {
 	Code string `json:"code" binding:"required"`
+	Role string `json:"role" binding:"required"`
 }
 
 func (dto SigninGoogleDTO) Validate() error {
