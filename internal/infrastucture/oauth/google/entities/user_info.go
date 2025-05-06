25ce505b9328a3117d81555260cafd98c844bd79
rafaelzrm <rafaelzrm@getpepper.ai>
2025-05-06 01:19:22 -0600

    feat: implement OAuth Google user info retrieval and token handling

---
+++ internal/infrastucture/oauth/google/entities/user_info.go
@@ -0,0 +1,11 @@
+package entities
+
+type UserInfo struct {
+	Email         string `json:"email"`
+	Name          string `json:"name"`
+	GivenName     string `json:"given_name"`
+	FamilyName    string `json:"family_name"`
+	Picture       string `json:"picture"`
+	VerifiedEmail bool   `json:"verified_email"`
+	Locale        string `json:"locale"`
+}
