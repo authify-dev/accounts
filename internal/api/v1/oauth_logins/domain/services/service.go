0b8230b9a8e7e4f35fdbbc94e358d98f61bcfcc3
reitmas32 <rafa.zamora.rals@gmail.com>
2025-05-07 21:17:05 -0600

    feat: refactor OAuthService to improve repository imports and structure

--- internal/api/v1/oauth_logins/domain/services/service.go
+++ internal/api/v1/oauth_logins/domain/services/service.go
@@ -1,18 +1,40 @@
 package services
 
-import "accounts/internal/api/v1/oauth_logins/domain/repositories"
+import (
+	login_methods "accounts/internal/api/v1/login_methods/domain/repositories"
+	oauth_logins "accounts/internal/api/v1/oauth_logins/domain/repositories"
+	refresh_tokens "accounts/internal/api/v1/refresh_tokens/domain/repositories"
+	roles "accounts/internal/api/v1/roles/domain/repositories"
+	users "accounts/internal/api/v1/users/domain/repositories"
+	"accounts/internal/common/controllers"
+)
 
 type OAuthService struct {
-	google_repository repositories.OauthClientRepository
-	oauth_repository  repositories.OAuthLoginRepository
+	google_repository       oauth_logins.OauthClientRepository
+	oauth_repository        oauth_logins.OAuthLoginRepository
+	user_repository         users.UserRepository
+	role_repository         roles.RoleRepository
+	login_method_repository login_methods.LoginMethodRepository
+	refresh_repository      refresh_tokens.RefreshTokenRepository
+	jwt_controller          controllers.JWTController
 }
 
 func NewOAuthService(
-	google_repository repositories.OauthClientRepository,
-	oauth_repository repositories.OAuthLoginRepository,
+	google_repository oauth_logins.OauthClientRepository,
+	oauth_repository oauth_logins.OAuthLoginRepository,
+	user_repository users.UserRepository,
+	role_repository roles.RoleRepository,
+	login_method_repository login_methods.LoginMethodRepository,
+	refresh_repository refresh_tokens.RefreshTokenRepository,
+	jwt_controller controllers.JWTController,
 ) *OAuthService {
 	return &OAuthService{
-		google_repository: google_repository,
-		oauth_repository:  oauth_repository,
+		google_repository:       google_repository,
+		oauth_repository:        oauth_repository,
+		user_repository:         user_repository,
+		role_repository:         role_repository,
+		login_method_repository: login_method_repository,
+		refresh_repository:      refresh_repository,
+		jwt_controller:          jwt_controller,
 	}
 }
